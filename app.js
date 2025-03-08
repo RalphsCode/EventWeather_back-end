57441c77504618c4e6c850f837d8520437cfbedc
RalphsCode <RalphsCode@gmail.com>
2025-03-07 23:02:54 -0800

    Added some comments

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors'); 
+const express = require('express');     // npm i express
+const axios = require('axios');         // npm i axios
+const cors = require('cors');           // npm i cors
 const app = express();
 
 // Enable CORS for all routes
 app.use(cors());
 
+// Google Maps Route
 app.get('/api/google', async function (req, res){
     try {
+        // Get the url string paramater
         const location = req.query.input;
         if (!location) {
             return res.status(400).json({ error: "Missing Location" });
           }
           
-          const keyString = process.env.REACT_APP_GOOGLE_KEY;
+          // get the API key from .env
+          const keyString = process.env.GOOGLE_KEY;
           if (!keyString) {
             return res.status(500).json({ error: "API key not configured" });
         }
 
+          // Make the API call
           const response = await axios.get(
             `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${location}&key=${keyString}`
           );
           
+          // Separate out the needed data
           const result = response.data.results[0];
           const locationData =  {
                     location: result.name,
@@ -40,6 +45,7 @@ app.get('/api/google', async function (req, res){
     }
 
 })  // END route
+
 
 // Start a server
 app.listen(3001, function(){
